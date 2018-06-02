1e1cfde28ba564b9baf1da8c4929dd86585bf2f9
shinshin86 <beagles1986@gmail.com>
2018-06-02 11:42:30 +0900

    Add

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # notice-googlespreadsheets-schedule-to-slack
+
 Notice Google Spread Sheets Schedule To Slack
+
+
+
+## Usage
+
+1. Write your schedule at Google spread sheets
+
+![example image 01](./img/001.png)
+
+
+
+2. Setup Google apps script ([here](./scripts/notice.js))  This spread sheet. And Please set it to run automatically at your optional time.
+3. Your will find Notification to Slack!
+
+![sample image 02](./img/002.png)
---
+++ scripts/notice.js
@@ -0,0 +1,98 @@
+/**
+ * send to Slack
+ * @param {object} data  send data
+ * @param {object} url   webhook url
+ */
+function sendToSlack(data, url) {
+  var payload = JSON.stringify(data);
+  var options = {
+    "method" : "POST",
+    "contentType" : "application/json",
+    "payload" : payload
+  };
+  UrlFetchApp.fetch(url, options);
+}
+
+/**
+ * Make send data
+ * @param  {string} username : bot name
+ * @param  {string} channelName : send slack channe name
+ * @param  {string} icon : icon(emoji name)
+ * @param  {string} text : text
+ * @param  {list} attachments: attachments data
+ * @return {object} json data : send data for slack
+ */
+function makeSendData(username,
+                      channelName,
+                      icon,
+                      text,
+                      attachments){
+  
+  var jsonData = {
+    username: username,
+    channel: channelName,
+    icon_emoji: icon,
+    text: text,
+    attachments: attachments
+  }
+  return jsonData;
+}
+
+/**
+ * Make atttachment
+ * @param {string} name : payment username
+ * @param {object} data : payment data
+ * @param  {string} color : attachmenet color code ex) #aaaaaa
+ * @return {object} attachment : attachment data
+ */
+function makeAttachment(name, data, color) {
+  return {
+    title: name,
+    text: data,
+    color: color
+  }
+}
+
+function getThisMonthSheetName() {
+  var now = new Date();
+  var year = now.getFullYear();
+  var month = now.getMonth() + 1;
+  var res = year + '/' + ('00' + month).slice(-2);
+  return res;
+}
+
+function noticeWork() {
+  // spreadsheet
+  var spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
+  var sheet = spreadsheet.getSheetByName(getThisMonthSheetName());
+  var values = sheet.getDataRange().getValues();
+  
+  var dt = new Date();
+  var today = dt.getDate();
+
+  var worktimes = '';  
+
+  for(var i = today; i < (today + 5); i++) {
+    if(values.length < i) {
+      res += "翌月分の仕事スケジュールが入力されていないようです。\n";
+      break;
+    } else {
+      worktimes += (values[i][0].getMonth() + 1) + "/" + values[i][0].getDate() + " : ";
+      worktimes += values[i][1];
+      worktimes += "\n";
+    }
+  }
+
+  var attachments = []
+  attachments.push(makeAttachment("5日先までの仕事スケジュールを記載しています", worktimes, "#1e90ff"));
+  
+  /*** Please rewrite freely according to your taste. ***/
+  var jsonData = makeSendData("お仕事時間報告BOT",
+               "#your specific channel",
+               ":memo:",
+               "{UserName}さんのお仕事スケジュールを報告します💁",
+               attachments
+              )
+  var POST_URL = "" // slack webhook URL
+  sendToSlack(jsonData, POST_URL);
+}
